a658358d7ea87467ff67ac2219ccf60dba0ba617
sjzlyl406 <sjzlyl406@163.com>
2015-10-05 15:35:25 +0800

    python_contrl

---
+++ python_contrl/my.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+from smtp import smtpfuc
+
+if __name__ == '__main__':
+	ret = os.system('ping -c3 www.baidu.com')
+	count = 3
+	while ret != 0 and count != 0:
+		ret = os.system('ping -c3 www.baidu.com')
+		count -= 1
+	if count == 0:
+		print 'fail'
+	else:
+		smtpfuc()
